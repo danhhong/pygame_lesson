605e02815c9d5a6634974637560eaee049688795
khmertool <46778258+khmertool@users.noreply.github.com>
2021-01-23 14:46:30 +0700

    update

--- 4_game_over.py
+++ 4_game_over.py
@@ -22,7 +22,6 @@ surface.fill(black)
 image = pygame.image.load(r'image/whitebird.png')
 
 # prepare for game over function
-
 def replay_or_quit():
     for event in pygame.event.get([pygame.KEYDOWN, pygame.KEYUP, pygame.QUIT]):
         if event.type == pygame.QUIT:
@@ -41,15 +40,15 @@ def makeTextObjs(text, font):
     textSurface = font.render(text, True, white)
     return textSurface, textSurface.get_rect()
 
-def msgSurface(text):
+def msgSurface(title, subtitle):
     small_text = pygame.font.Font(None, 20)
     large_text = pygame.font.Font(None, 150)
     
-    titleTextSurf, titleTextRect = makeTextObjs(text, large_text)
+    titleTextSurf, titleTextRect = makeTextObjs(title, large_text)
     titleTextRect.center = width / 2, height / 2
     surface.blit(titleTextSurf, titleTextRect)
     
-    typTextSurf, typTextRect = makeTextObjs('Press any key to continue', small_text)
+    typTextSurf, typTextRect = makeTextObjs(subtitle, small_text)
     typTextRect.center = width / 2, ((height / 2) + 100)
     surface.blit(typTextSurf, typTextRect)
     
@@ -62,8 +61,8 @@ def msgSurface(text):
     main()
 
 # over funcion
-def gameOver():
-    msgSurface('Game Over')
+def gameOver(title, subtitle):
+    msgSurface(title, subtitle)
     
 def main():
     x, y = 100, 100
@@ -91,7 +90,7 @@ def main():
         
         # use game over function
         if y > height-40 or y < 0:
-            gameOver()
+            gameOver('Game Over', 'Press any key to continue')
         
         pygame.display.update()
         clock.tick(60)
