1e90982f60c65c4f1588c2bd157ccfdcd03ff9d5
khmertool <46778258+khmertool@users.noreply.github.com>
2021-01-23 16:14:02 +0700

    update

--- draw_rect.py
+++ draw_rect.py
@@ -8,6 +8,15 @@ clock = pygame.time.Clock()
 
 game_over = False
 
+# variable for rectangle
+color = (255,0,0) # red
+x, y = 200, 100
+rect_width, rect_height = 50, 200
+
+# draw rectangle
+pygame.draw.rect(surface, color, [x, y, rect_width, rect_height])
+
+
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- draw_rectangle.py
+++ draw_rectangle.py
@@ -3,10 +3,18 @@ import pygame
 pygame.init()
 width, height = 800, 400
 surface = pygame.display.set_mode((width, height))
-pygame.display.set_caption('My First Game')
+pygame.display.set_caption('Draw Rectangle')
 clock = pygame.time.Clock()
 
 game_over = False
+
+# aviable for rectangle
+color = (0, 0, 255) # red
+x, y = 100, 50
+rect_width, rect_height = 300, 200
+
+# draw rectangle
+pygame.draw.rect(surface, color, [x, y, rect_width, rect_height])
 
 while not game_over:
     for event in pygame.event.get():
